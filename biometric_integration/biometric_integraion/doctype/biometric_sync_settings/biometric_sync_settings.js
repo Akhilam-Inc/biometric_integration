9ac7506f79edf78f67e9b2f64ce3d59d88b17eb1
parthdungarani <parthdungarani@gmail.com>
2025-12-10 14:19:14 +0530

    module correction

--- biometric_integration/biometric_integraion/doctype/biometric_sync_settings/biometric_sync_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Akhilam Inc. and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Biometric Sync Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
